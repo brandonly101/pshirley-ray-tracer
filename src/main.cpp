7ae76619186ed571b49a4bbb0991f27e639e9b1a
Brandon Ly <6823589+brandonly101@users.noreply.github.com>
2019-07-03 16:26:27 -0700

    Added writing to PNG file - Added compiler optimizations for Windows; need to check for Mac (and Linux)

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 #include <cfloat>
 #include <cstdlib>
 #include <thread>
+#include <ctime>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 
 #include "vec3.h"
 #include "ray.h"
@@ -13,9 +17,16 @@
 
 using namespace std;
 
-#define nx 960
-#define ny 540
-#define ns 100
+// Constants that define properties of this ray tracer.
+#define N_X 1280
+#define N_Y 720
+#define N_CHANNELS 4
+#define N_S 100
+
+double getRand()
+{
+	return double(rand()) / double(RAND_MAX);
+}
 
 vec3 getColor(const ray& r, Hitable* world)
 {
@@ -42,39 +53,45 @@ void processPixels(int start,
                    int end,
                    Camera* cam,
                    Hitable* world,
-                   vector<string>* pixels)
+                   vector<unsigned char>* pixels)
 {
     for (int iter = start; iter < end; iter++)
     {
-        const int i = iter % nx;
-        const int j = ny - iter / nx - 1;
+        const int i = iter % N_X;
+        const int j = N_Y - iter / N_X - 1;
 
         vec3 col;
 
-        for (int s = 0; s < ns; s++)
+        for (int s = 0; s < N_S; s++)
         {
-            float u = float(i + drand48()) / float(nx);
-            float v = float(j + drand48()) / float(ny);
+            float u = float(i + getRand()) / float(N_X);
+            float v = float(j + getRand()) / float(N_Y);
             ray r = cam->getRay(u, v);
             vec3 p = r.pointAtParameter(2.0);
             col += getColor(r, world);
         }
 
-        col /= float(ns);
+        col /= float(N_S);
 
         int ir = int(255.99 * sqrt(col[0]));
         int ig = int(255.99 * sqrt(col[1]));
         int ib = int(255.99 * sqrt(col[2]));
 
-        (*pixels)[iter] = to_string(ir) + " " + to_string(ig) + " " + to_string(ib);
+		(*pixels)[iter * N_CHANNELS + 0] = (unsigned char)ir;
+		(*pixels)[iter * N_CHANNELS + 1] = (unsigned char)ig;
+		(*pixels)[iter * N_CHANNELS + 2] = (unsigned char)ib;
+		if (N_CHANNELS == 4)
+		{
+			(*pixels)[iter * N_CHANNELS + 3] = 0xFF;
+		}
     }
 }
 
 int main(int argc, char const *argv[])
 {
-    cout << "P3" << endl;
-    cout << to_string(nx) + " " + to_string(ny) << endl;
-    cout << "255" << endl;
+	clock_t begin = clock();
+
+	cout << "Ray tracing image ..." << endl;
 
     vector<Hitable*> list;
     list.push_back(new Sphere(vec3(0, 0, -1), 0.5));
@@ -82,30 +99,40 @@ int main(int argc, char const *argv[])
 
     HitableList world(list);
     Camera cam;
-    vector<string> pixels(nx * ny, "0 0 0");
+    vector<unsigned char> pixels(N_X * N_Y * N_CHANNELS, 0);
 
-    // processPixels(0, nx * ny, &cam, (Hitable*)&world, &pixels);
-
-    const unsigned NUM_THREADS = thread::hardware_concurrency();
-
-    vector<thread> threads(NUM_THREADS);
+	// Create and kick off threads for subsections of pixels.
+	const int NUM_THREADS = int(thread::hardware_concurrency());
+	vector<thread> threads(NUM_THREADS);
     for (int i = 0; i < NUM_THREADS; i++)
     {
         threads[i] = thread(
             processPixels,
-            (i * nx * ny) / (int)NUM_THREADS,
-            ((i + 1) * (nx * ny)) / (int)NUM_THREADS,
+            (i * N_X * N_Y) / (int)NUM_THREADS,
+            ((i + 1) * (N_X * N_Y)) / (int)NUM_THREADS,
             &cam,
             (Hitable*)&world,
             &pixels
         );
     }
-    for (int i = 0; i < NUM_THREADS; i++)
+
+	// Block until all threads are complete.
+	for (int i = 0; i < NUM_THREADS; i++)
     {
         threads[i].join();
     }
 
-    for (string& s : pixels) cout << s << endl;
+	// Write to PNG file
+	int x = N_X;
+	int y = N_Y;
+	int n = N_CHANNELS;
+	stbi_write_png("image.png", x, y, n, pixels.data(), 0);
+
+	cout << "Done!" << endl;
+
+	clock_t end = clock();
+	double elapsedSecs = double(end - begin) / CLOCKS_PER_SEC;
+	cout << "Time elapsed: " + to_string(int(elapsedSecs) / 60) + "m" + to_string(int(elapsedSecs) % 60) + "s" << endl;
 
     return 0;
 }
